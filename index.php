a831b80a8b4c75c4c77b54341d18f86ac1fa28c6
MohamedkaniH <145865528+MohamedkaniH@users.noreply.github.com>
2024-04-19 21:10:14 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,190 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Inventory Management System || Home Page</title>
+
+<!-- Custom Theme files -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/fasthover.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all" />
+<!-- //Custom Theme files -->
+<!-- font-awesome icons -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- //font-awesome icons -->
+<!-- js -->
+<script src="js/jquery.min.js"></script>
+<link rel="stylesheet" href="css/jquery.countdown.css" /> <!-- countdown --> 
+<!-- //js -->  
+<!-- web fonts --> 
+<link href='//fonts.googleapis.com/css?family=Glegoo:400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+<!-- //web fonts -->  
+<!-- start-smooth-scrolling -->
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- //end-smooth-scrolling --> 
+</head> 
+<body>
+	<!-- for bootstrap working -->
+	<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
+	<!-- //for bootstrap working -->
+	
+
+	<!-- header -->
+	<div class="header" id="home1">
+		<div class="container">
+		
+			<div class="w3l_logo">
+				<h1><a href="index.php">Inventory Management System</a></h1>
+			</div>
+		
+		</div>
+	</div>
+	<!-- //header -->
+	<!-- navigation -->
+	<div class="navigation">
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header nav_2">
+					<button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div> 
+				<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+					<ul class="nav navbar-nav">
+						<li ><a href="index.php" class="act">Home</a></li>	
+						
+						<li><a href="admin/login.php">Admin</a></li> 
+					  
+						
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</div>
+	<!-- //navigation -->
+	<!-- banner -->
+	<div class="banner">
+		<div class="container">
+			<h3>Inventory Management, <span>System</span></h3>
+		</div>
+	</div>
+	<!-- //banner --> 
+	<div class="container ">
+	<div class="row mt-5" style="padding-top: 50px;">
+		<div class="col-lg-6 col-md-8">
+			<h3 style="color:#3c43a4;"><strong>About Inventory Management System</strong> </h3>
+		<div style="line-height: 25px; font-size:20px;padding-top:35px">
+		<p>
+		An inventory management system (IMS) is a software system that tracks and controls stock before it is sold. It can help businesses reduce inventory carrying costs while ensuring that there is enough stock to meet customer demand. IMS can also be used in the manufacturing industry to create work orders, bills of materials, and other production-related documents.
+		</p>
+		<p>
+			Inventory all your IT and non-IT assets, including your software licenses, with the native IT asset management capabilities of ServiceDesk Plus. Maximize the visibility of your hardware and software assets with multiple discovery modes like Windows domain scan, script scan, agent-based scan, and barcode scanning.
+			</p>
+			<p style="padding-top: 10px;">
+			Effectively inventory all your hardware and software assets by defining custom product types, products, and product groups. Also, keep track of the key asset financials including their depreciation information. Generate custom reports on your IT asset inventory for effective planning and budgeting.
+			</p>
+		
+			</div>
+		</div>
+		<div class="col-lg-6 col-md-4">
+			<img src="images/about.png" >
+		</div>
+	</div>
+	</div>
+	<div class="container" style="padding-top: 50px;" >
+	<div class="row">
+	<div class="col-lg-6 col-md-6">
+	<h3 style="color:#3c43a4;"><strong>Vision </strong> </h3>
+		<div style="line-height: 25px; font-size:20px;padding-top:35px">
+		<p>
+		Vision Inventory Management is a fully-hosted, multi-level vendor managed inventory and inventory management solution that provides real-time visibility, forecasting, and reporting across the supply chain. It offers inventory visibility and demand planning to manufacturers and retailers, enabling them to respond quickly.
+		</p>
+		<p>
+		Inventory management helps to ensure that there is enough goods or materials to meet demand without creating overstock, or excess inventory. Some objectives of inventory management include: Having material available, Improve order fulfillment, Having the right amount of stock, Keeping wastage and losses to a minimum, and Increase profit.
+		</p>
+		</div>
+	</div>
+	<div class="col-lg-6 col-md-6">
+	<h3 style="color:#3c43a4;"><strong>Mission </strong> </h3>
+		<div style="line-height: 25px; font-size:20px;padding-top:35px">
+		<p>
+		An inventory management system (IMS) helps businesses manage their inventory levels, orders, sales, and deliveries. It can also be used to create receipts, work-orders, bill of materials, and other documents. An IMS can track goods and resources throughout their life cycle, allowing companies to identify bottlenecks and fix issues before they start to accumulate.
+		</p>
+		<p>
+		The mission of an IMS is to provide a central repository of information used to define assets and relate them to their owner, location, and relative importance. This information provides personnel with data needed to support their job functions.
+		</p>
+		</div>
+	</div>
+	</div>
+	</div>
+	<div class="container" style="padding-top: 50px;" >
+	<div class="row">
+	<div class="col-lg-12 col-md-12">
+	<h3 style="color:#3c43a4;"><strong>Why is inventory management important? </strong> </h3>
+		<div style="line-height: 25px; font-size:20px;padding-top:35px">
+		<p>
+		One of the most valuable assets of a company is its inventory. In various industries, such as retail, food services, and manufacturing, a lack of inventory can have detrimental effects. Aside from being a liability, inventory can also be considered a risk. It can be prone to theft, damage, and spoilage. Having a large inventory can also lead to a reduction in sales.
+			</p>
+		<p>
+		Both for small businesses and big corporations, having a proper inventory management system is very important for any business. It can help you keep track of all your supplies and determine the exact prices. It can also help you manage sudden changes in demand without sacrificing customer experience or product quality. This is especially important for brands looking to become a more customer-centric organization. 
+			</p>
+			<p>
+			Balancing the risks of overstocks and shortages is an especially challenging process for companies with complex supply chains. A company's inventory is typically a current asset that it plans to sell within a year. It must be measured and counted regularly to be considered a current asset. 
+			</p>
+			<p>
+			The goal of any good inventory management system is to help warehouse managers keep track of the stock levels of their products. This means allowing them full transparency into their chain to monitor the flow of goods from their supplier. 
+
+The benefits are both operational and financial. Not only will it serve to improve performance, but it’s also useful for preventing theft with the help of product tracking and security. 
+			</p>
+		</div>
+	</div>
+
+	</div>
+	</div>
+	
+	<div class="footer">
+		
+		<div class="footer-copy" >
+			<div class="footer-copy1">
+				<div class="footer-copy-pos">
+					<a href="#home1" class="scroll"><img src="images/arrow.png" alt=" " class="img-responsive" /></a>
+				</div>
+			</div>
+			<div class="container">
+				<p>Inventory Management System @ 2024</p>
+			</div>
+		</div>
+	</div>
+	<!-- //footer --> 
+	<!-- cart-js -->
+	<script src="js/minicart.js"></script>
+	<script>
+        w3ls.render();
+
+        w3ls.cart.on('w3sb_checkout', function (evt) {
+        	var items, len, i;
+
+        	if (this.subtotal() > 0) {
+        		items = this.items();
+
+        		for (i = 0, len = items.length; i < len; i++) { 
+        		}
+        	}
+        });
+    </script>  
+	<!-- //cart-js -->   
+</body>
+</html>
